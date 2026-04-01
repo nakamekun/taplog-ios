356f1f8130c13f86189beb1266bf3e39854cd094
ZEC <villagepaddy@gmail.com>
2026-04-01 23:53:00 +0900

    Initial Commit

---
+++ TapLog/TapLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapLogApp.swift
+//  TapLog
+//
+//  Created by KEITA TAMURA on 2026/04/01.
+//
+
+import SwiftUI
+
+@main
+struct TapLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
